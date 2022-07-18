ce249c7d6d7e4890cd58d82d3292e275dd1205da
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-18 16:25:57 +0200

    uses Provider giving access to the contexts

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'screens/login.dart';
+import 'blocs/provider.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   build(context) {
-    return const MaterialApp(
-        title: 'Log Me In', home: Scaffold(body: LoginScreen()));
+    // Provider creates a new instance of a Bloc,
+    // everything beneath can access the contexts
+    return Provider(
+      child: const MaterialApp(
+          title: 'Log Me In', home: Scaffold(body: LoginScreen())),
+    );
   }
 }
