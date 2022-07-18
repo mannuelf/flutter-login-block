0070ab8a2ce86e7ef7671dd309dc7d14d83e273b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-18 13:34:17 +0200

    add inputs and buttons

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'screens/login.dart';
+
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   build(context) {
-    return MaterialApp(title: 'Log Me In', home: Scaffold(body: LoginScreen));
+    return const MaterialApp(
+        title: 'Log Me In', home: Scaffold(body: LoginScreen()));
   }
 }
--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
   Widget build(context) {
     return Container(
-      margin: EdgeInsets.all(20.0),
+      margin: const EdgeInsets.all(20.0),
       child: Column(
-        children: [],
+        children: <Widget>[emailField(), passwordField(), submitButton()],
       ),
     );
+  }
+
+  Widget emailField() {
+    return const TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          hintText: 'you@example.com', labelText: 'Add Email Address'),
+    );
+  }
+
+  Widget passwordField() {
+    return const TextField(
+      obscureText: true,
+      decoration: InputDecoration(hintText: 'Password', labelText: 'Password'),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+        onPressed: () => print('Logging in'), child: Text('Login'));
   }
 }
